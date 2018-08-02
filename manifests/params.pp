755ac63105b92502fe3d1439ddfeccbfb5c0175a
Elyse Salberg <elyse_salberg@putnam.com>
2018-08-02 16:32:36 -0400

    Add parameters to control management of target directory and unzip package Add groovy.csh file for environment defaults

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,8 +1,10 @@
 #
 #  default parameters for the groovy class
 class groovy::params {
-  $version  = '2.3.1'
-  $base_url = 'http://dl.bintray.com/groovy/maven/'
-  $target   = '/opt'
-  $timeout  = 360
+  $version        = '2.3.1'
+  $base_url       = 'http://dl.bintray.com/groovy/maven/'
+  $target         = '/opt'
+  $manage_target  = true
+  $manage_unzip   = true
+  $timeout        = 360
 }
